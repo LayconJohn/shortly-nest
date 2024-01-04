11e07b025f61e3e4f55832f94220ed0d259a1e50
Laycon John <laycon.gomes@tld.local>
2024-01-04 09:50:32 -0300

    chore(users): update CreateUserdto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+
+    @MaxLength(30)
+    @MinLength(8)
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
