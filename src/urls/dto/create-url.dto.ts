961de8313252064cc44988bcc473af1cc374e730
Laycon John <laycon.gomes@tld.local>
2024-01-04 17:28:15 -0300

    feat(url): create url dto

--- src/urls/dto/create-url.dto.ts
+++ src/urls/dto/create-url.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUrlDto {}
+import { PartialType } from "@nestjs/mapped-types";
+import { Url } from "../entities/url.entity";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUrlDto extends PartialType(Url){
+    @IsString()
+    @IsNotEmpty()
+    url: string;
+}
