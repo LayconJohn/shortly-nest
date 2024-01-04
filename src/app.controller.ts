0a6a1d28322a0bde5dd6bd243247c279eba70a76
Laycon John <laycon.gomes@tld.local>
2024-01-04 16:13:38 -0300

    feat(app): create get me with decorator currentUser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { isPublic } from './auth/decorator/is-public.decorator';
+import { CurrentUser } from './auth/decorator/current-user.decorator';
+import { User } from './users/entities/User';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @isPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() currentUser: User) {
+    return currentUser;
   }
 }
