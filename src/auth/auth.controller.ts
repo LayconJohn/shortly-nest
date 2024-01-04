9b09faf07f96de138db5ff6b6febb47f65a70726
Laycon John <laycon.gomes@tld.local>
2024-01-04 14:30:11 -0300

    feat(auth): imoplements decoretor isPublic on auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpCode, HttpStatus, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthRequest } from './models/AuthRequest';
+import { isPublic } from './decorator/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
-    //Criar decorator isPublic
+    @isPublic()
     //Criar use guard com local auth guard 
     @Post('login')
     @HttpCode(HttpStatus.OK)
