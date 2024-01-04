b36946bd7b5388272fbe0d2b6d1bc0fc0dcb8c36
Laycon John <laycon.gomes@tld.local>
2024-01-04 13:22:06 -0300

    chore: create AuthRequest model

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login(@Request() req: AuthRequest) {}
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: '10d'},
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, ]//Resta criar LocalStrategy e JwtStrategy
 })
-export class AuthModule {}
+
+export class AuthModule implements NestModule{
+  //Resta implementar LoginValidationMiddleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply().forRoutes('login')
+  }
+}
