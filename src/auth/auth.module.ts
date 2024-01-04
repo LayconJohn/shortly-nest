6a1c12b1a7ef26a410d1652e1606dec4ef78a337
Laycon John <laycon.gomes@tld.local>
2024-01-04 15:20:53 -0300

    feat(auth): create LoginValidationMiddleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { UsersService } from 'src/users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { LoginValidationMiddleware } from 'src/middlewares/login-validation.middlewares';
 
 @Module({
   imports: [
@@ -18,12 +19,12 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy]//Resta criar LocalStrategy e JwtStrategy
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 
 export class AuthModule implements NestModule{
   
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply().forRoutes('login') //Resta implementar LoginValidationMiddleware
+    consumer.apply(LoginValidationMiddleware).forRoutes('login') 
   }
 }
