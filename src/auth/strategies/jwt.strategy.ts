afec1f3d90a11f0af8bf19ee83a0e12526719020
Laycon John <laycon.gomes@tld.local>
2024-01-05 10:52:16 -0300

    feat(auth): change in jwt.strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException  } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/entities/User';
+import { UsersService } from 'src/users/users.service';
+import { JwtPayload } from './jwt-payload';
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,11 +16,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any): Promise<any> {
-    return {
-      id: payload.sub,
-      email: payload.email,
-      name: payload.name,
-    };
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
